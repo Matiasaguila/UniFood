96045a06e2b7f28e1fc6a7f34e5012ffec3846e3
freelancersazal <freelancersazal@gmail.com>
2022-11-04 16:58:18 +0600

    added five new pages

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ $(function () {
     //=========NICE SELECT=========
     $('#select_js').niceSelect();
     $('#select_js2').niceSelect();
+    $('#select_js3').niceSelect();
 
 
 
@@ -223,8 +224,6 @@ $(function () {
         autoplaySpeed: 2000,
         dots: false,
         arrows: false,
-        // nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
-        // prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
 
         responsive: [
             {
@@ -331,6 +330,54 @@ $(function () {
             }
         ]
     });
+
+
+
+    //=======OFFER ITEM SLIDER====== 
+    $('.related_product_slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-long-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-long-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
 
 
 
